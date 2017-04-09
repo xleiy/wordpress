02ff9c5b1bf123da05246d3af06019ce129e2209
xleiyao <xlei1139987950@gmail.com>
2017-04-09 10:28:57 +0800

    add baidu search

--- wp-content/themes/xlei/header.php
+++ wp-content/themes/xlei/header.php
@@ -43,23 +43,12 @@
         <?php endif;?>
         <div>
             <div class="site-nav" style=""><span class="button-toggle"></span>
-               <!--  <div class="container">
-                    <div class="nav-item <?php if (is_home()) { echo 'current';} ?>"><a title="<?php bloginfo('name'); ?>"  href="<?php echo get_option('home'); ?>/">主页</a></div>
-                    <div class="nav-item"><a href="/archives" target="_self" data-text="archive">归档</a></div>
-                    <div class="nav-item"><a href="/about" target="_self" data-text="about">关于</a></div>
-                    <div class="nav-item"><a href="/atom.xml" target="_self" data-text="rss">订阅</a></div>
-                    <div class="nav-item"><a href="/resume" target="_blank">求职</a></div>
-                </div> -->
                  <div class="container">
                     <div class="nav-item <?php if (is_home()) { echo 'current';} ?>">
                         <a target="_self" title="<?php bloginfo('name'); ?>" href="<?php echo get_option('home'); ?>/">主页</a>
                     </div>
                      <?php wp_list_pages('depth=1&title_li=0&sort_column=menu_order'); ?>
                 </div>
-                <!-- <ul id="navigation" class="grid_8">
-                    <?php wp_list_pages('depth=1&title_li=0&sort_column=menu_order'); ?>
-                    <li <?php if (is_home()) { echo 'class="current"';} ?>><a title="<?php bloginfo('name'); ?>"  href="<?php echo get_option('home'); ?>/">主页</a></li>
-                </ul> -->
                 <?php get_search_form(); ?>
             </div>
         </div>
